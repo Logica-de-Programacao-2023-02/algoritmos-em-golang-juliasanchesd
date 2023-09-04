9b70dea6552b5214c55a9343021174198ec7c82c
Júlia Sanches <141031378+juliasanchesd@users.noreply.github.com>
2023-09-04 08:26:25 -0300

    Update resolução - repetição 09.go

--- resolução - repetição 09.go
+++ resolução - repetição 09.go
@@ -1,1 +1,17 @@
 package main
+
+import "fmt"
+
+func main() {
+	var soma, quantidade, x int
+	x = -1
+	for x != 0 {
+		fmt.Print("Digite um número: ")
+		fmt.Scan(&x)
+
+		soma += x
+		quantidade++
+	}
+	media := soma / quantidade
+	fmt.Println("A média é ", media)
+}
