fbe1671381a4de613f38f6461c573796bfb4144b
Júlia Sanches <141031378+juliasanchesd@users.noreply.github.com>
2023-09-04 08:35:30 -0300

    Update resolução - repetição 10.go

--- resolução - repetição 10.go
+++ resolução - repetição 10.go
@@ -1,1 +1,17 @@
 package main
+
+import "fmt"
+
+func main() {
+	var maior, x int
+	x = -1
+	for x != 0 {
+		fmt.Print("Digite um número: ")
+		fmt.Scan(&x)
+
+		if x > maior {
+			maior = x
+		}
+	}
+	fmt.Println("O maior é ", maior)
+}
